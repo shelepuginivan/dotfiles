a0f6e8c0ed48a79651d4e07ab43218ba587a6983
shelepuginivan <shelepuginivanm@gmail.com>
2024-01-08 10:34:48 +0300

    feat(zsh, apps): add cargo installation in `$PATH`

--- .config/zsh/apps.zsh
+++ .config/zsh/apps.zsh
@@ -3,6 +3,10 @@ export BUN_INSTALL="$XDG_CONFIG_HOME/bun"
 export PATH="$PATH:$BUN_INSTALL/bin"
 [ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
 
+# --- Cargo ---
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export PATH="$PATH:$CARGO_HOME/bin"
+
 # --- Lua Language Server ---
 export LUA_LS_DIR="$XDG_CONFIG_HOME/lua-ls"
 export PATH="$PATH:$LUA_LS_DIR/bin"
