80746fa653573353579a8bcc5409588194a4b130
shelepuginivan <shelepuginivanm@gmail.com>
2024-05-23 22:11:11 +0300

    feat(zsh): remove icons in `/dev/tty*`

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -13,3 +13,12 @@ zstyle ':vcs_info:*:*' check-for-changes true
 
 PROMPT='%B %(?.%F{green}.%F{red})%f %F{cyan}%1~%f %b'
 RPROMPT='%F{8}$vcs_info_msg_0_%f'
+
+# Remove icons if running in a tty.
+if [[ "$(tty)" =~ "/dev/tty*" ]]; then
+    zstyle ':vcs_info:git*' actionformats '%F{14}@ %*%f'
+    zstyle ':vcs_info:git*' formats 'git %b%F{yellow}%u%f%F{green}%c%f'
+    zstyle ':vcs_info:git*' stagedstr ' +'
+    zstyle ':vcs_info:git*' unstagedstr ' *'
+    PROMPT='%B %(?.%F{green}OK.%F{red}ERR)%f %F{cyan}%1~%f %b'
+fi
