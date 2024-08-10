20f4e40cdd607151e2f5f730cabac08f84a19623
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-10 18:06:12 +0300

    refactor(zsh): organize app configs and enhance docs

--- .config/zsh/apps.zsh
+++ .config/zsh/apps.zsh
@@ -1,37 +1,39 @@
 #!/usr/bin/env zsh
 
-# --- bun ---
+# ────────────────────────────────────────────────────┤ 1. Bun, Node.js, etc. │
 export BUN_INSTALL="$XDG_CONFIG_HOME/bun"
 export PATH="$PATH:$BUN_INSTALL/bin"
+export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
 [ -s "$BUN_INSTALL/_bun" ] && source "$BUN_INSTALL/_bun"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 
-# --- Docker ---
+
+# ────────────────────────────────────────────────────────────────┤ 2. Docker │
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 
-# --- Go ---
+
+# ────────────────────────────────────────────────────────────────────┤ 3. Go │
 export GOPATH="$XDG_DATA_HOME/go"
 export GOMODCACHE="$XDG_CACHE_HOME/go/mod"
 export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
 
-# --- Lua Language Server ---
+
+# ───────────────────────────────────────────────────────────────────┤ 4. Lua │
 export LUA_LS_DIR="$XDG_CONFIG_HOME/lua-ls"
 export PATH="$PATH:$LUA_LS_DIR/bin"
 
-# --- nvm ---
-export NVM_DIR="$XDG_CONFIG_HOME/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 
-# --- npm ---
-export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
-
-# --- Python ---
+# ────────────────────────────────────────────────────────────────┤ 5. Python │
 export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME/python"
 export PYTHONUSERBASE="$XDG_DATA_HOME/python"
 
-# --- Rust ---
+
+# ──────────────────────────────────────────────────────────────────┤ 6. Rust │
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
 export PATH="$PATH:$CARGO_HOME/bin"
 
-# --- wine ---
+
+# ──────────────────────────────────────────────────────────────────┤ 7. Wine │
 export WINEPREFIX="$XDG_DATA_HOME/wineprefixes/default"
