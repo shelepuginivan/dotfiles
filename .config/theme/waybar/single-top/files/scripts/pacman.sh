6027cd11f09a64e42cd8b756519c87dc90d17e21
shelepuginivan <shelepuginivanm@gmail.com>
2024-08-15 10:09:30 +0300

    fix(theme, waybar): proper output when updates are available

--- .config/theme/waybar/single-top/files/scripts/pacman.sh
+++ .config/theme/waybar/single-top/files/scripts/pacman.sh
@@ -3,11 +3,11 @@
 # Depends on pacman-contrib.
 
 UPDATE_LIST="$(checkupdates --nocolor --nosync)"
-UPDATES="Updates: $(printf $UPDATE_LIST | wc -l)"
+UPDATES="Updates: $(printf "$UPDATE_LIST" | wc -l)"
 
 if [ -z "$UPDATE_LIST" ]; then
     UPDATE_LIST="Arch-chan is happy!"
     UPDATES="Up to date!"
 fi
 
-echo "{\"text\":\"  $UPDATES\",\"tooltip\":\"$UPDATE_LIST\"}"
+printf "{\"text\":\"  $UPDATES\",\"tooltip\":\"$UPDATE_LIST\"}" | sed ':a;N;$!ba;s/\n/\\n/g'
